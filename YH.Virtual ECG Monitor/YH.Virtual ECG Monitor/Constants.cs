bf7a2acecd4070833a0296e0100b2e1f97c5b1e2
lindping <4830961@qq.com>
2018-11-21 17:52:53 +0800

    111

--- YH.Virtual ECG Monitor/YH.Virtual ECG Monitor/Constants.cs
+++ YH.Virtual ECG Monitor/YH.Virtual ECG Monitor/Constants.cs
@@ -11,40 +11,40 @@ namespace YH.Virtual_ECG_Monitor
         public const string GeneralWaveFile = "sound\\du.wav";
         public const string AlarmWaveFile = "sound\\du2.wav";
 
-        public static List<ListItem> MainWaveCategories = new List<ListItem>()
+        public static List<LayoutWave> MainWaveCategories = new List<LayoutWave>()
         {
-            new ListItem(){ Text="次要心电图导联", Value="次要心电图导联" },
-            new ListItem(){ Text="动脉血压", Value="动脉血压" },
-            new ListItem(){ Text="二氧化碳", Value="二氧化碳" },
-            new ListItem(){ Text="肺动脉压", Value="肺动脉压" },
-            new ListItem(){ Text="没有", Value="没有" },
-            new ListItem(){ Text="血氧饱和度", Value="血氧饱和度" },
-            new ListItem(){ Text="中心静脉压", Value="中心静脉压" },
-            new ListItem(){ Text="主要心电图导联", Value="主要心电图导联" }
+            new LayoutWave(){ Name="次要心电图导联", Status="开" },
+            new LayoutWave(){ Name="动脉血压",       Status="开" },
+            new LayoutWave(){ Name="二氧化碳",       Status="开" },
+            new LayoutWave(){ Name="肺动脉压",       Status="开" },
+            new LayoutWave(){ Name="没有",           Status="开" },
+            new LayoutWave(){ Name="血氧饱和度",     Status="开" },
+            new LayoutWave(){ Name="中心静脉压",     Status="开" },
+            new LayoutWave(){ Name="主要心电图导联", Status="开" }
         };
 
-        public static List<ListItem> otherWaveCategories = new List<ListItem>()
+        public static List<LayoutWave> otherWaveCategories = new List<LayoutWave>()
       {
-         new ListItem(){ Text="PH",          Value="PH" },
-         new ListItem(){ Text="PTC",         Value="PTC" },
-         new ListItem(){ Text="TOF",         Value="TOF" },
-         new ListItem(){ Text="颅内压",      Value="颅内压" },
-         new ListItem(){ Text="麻醉剂",      Value="麻醉剂" },
-         new ListItem(){ Text="脉搏",        Value="脉搏" },
-         new ListItem(){ Text="没有",        Value="没有" },
-         new ListItem(){ Text="气道呼吸率",  Value="气道呼吸率" },
-         new ListItem(){ Text="笑气",        Value="笑气" },
-         new ListItem(){ Text="心输出量",    Value="心输出量" },
-         new ListItem(){ Text="血温",        Value="血温" },
-         new ListItem(){ Text="血氧饱和度",  Value="血氧饱和度" },
-         new ListItem(){ Text="氧气",        Value="氧气" },
-         new ListItem(){ Text="周围温度",    Value="周围温度" },
+         new LayoutWave(){ Name="PH",          Status="开" },
+         new LayoutWave(){ Name="PTC",         Status="开" },
+         new LayoutWave(){ Name="TOF",         Status="开" },
+         new LayoutWave(){ Name="颅内压",      Status="开" },
+         new LayoutWave(){ Name="麻醉剂",      Status="开" },
+         new LayoutWave(){ Name="脉搏",        Status="开" },
+         new LayoutWave(){ Name="没有",        Status="开" },
+         new LayoutWave(){ Name="气道呼吸率",  Status="开" },
+         new LayoutWave(){ Name="笑气",        Status="开" },
+         new LayoutWave(){ Name="心输出量",    Status="开" },
+         new LayoutWave(){ Name="血温",        Status="开" },
+         new LayoutWave(){ Name="血氧饱和度",  Status="开" },
+         new LayoutWave(){ Name="氧气",        Status="开" },
+         new LayoutWave(){ Name="周围温度",    Status="开" },
       };
 
-        public static List<ListItem> NIBPWaveCategories = new List<ListItem>()
+        public static List<LayoutWave> NIBPWaveCategories = new List<LayoutWave>()
         {
-            new ListItem() { Text="没有",Value="没有" },
-            new ListItem() { Text="无创血压",Value="无创血压" }
+            new LayoutWave() { Name="没有",Status="开" },
+            new LayoutWave() { Name="无创血压",Status="开" }
         };
 
     }
@@ -53,6 +53,13 @@ namespace YH.Virtual_ECG_Monitor
     {
         男 = 0,
         女 = 1
+    }
+
+    public enum WaveInitStatus
+    {
+        开=0,
+        关=1,
+        空=2
     }
 
 
--- YH.Virtual ECG Monitor/YH.Virtual ECG Monitor/LayoutSetting.xaml.cs
+++ YH.Virtual ECG Monitor/YH.Virtual ECG Monitor/LayoutSetting.xaml.cs
@@ -1,16 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Shapes;
+
 
 namespace YH.Virtual_ECG_Monitor
 {
@@ -19,48 +11,190 @@ namespace YH.Virtual_ECG_Monitor
     /// </summary>
     public partial class LayoutSetting : Window
     {
-        List<ListItem> waveCategories;
+        List<LayoutWave> waveCategories;
         LayoutSettingData model;
+        LayoutWave selectedWave = null;
+        string layoutName = string.Empty;
+
+        int layoutSelectedIndex = -1;
+        int waveSelectedIndex = -1;
+        int waveCategoryIndex = -1;
+
         public LayoutSetting()
         {
             InitializeComponent();
+            InitializeData();
+        }
+
+        public void InitializeData()
+        {
             model = Setting.Get<LayoutSettingData>();
-            // lbWaveCategory.DataContext = waveCategories;
+            if (model == null || model.Layouts == null)
+            {
+                //没有数据则构建数据
+                model = new LayoutSettingData()
+                {
+                    Layouts = new List<LayoutSettingModel>() {
+                    new LayoutSettingModel() {  IsDefault=true, Name="5波形版面"},
+                    new LayoutSettingModel(){  IsDefault=true, Name="4波形版面"},
+                    new LayoutSettingModel(){  IsDefault=true, Name="3波形版面"},
+                    new LayoutSettingModel(){  IsDefault=true, Name="大数字版面"},
+                }
+                };
+
+
+                model.Layouts[0].MainWaveCategories = Constants.MainWaveCategories.Take(5).ToList();
+                model.Layouts[0].OtherWaveCategories = Constants.otherWaveCategories.Take(8).ToList();
+                model.Layouts[0].NIBPWaveCategory = Constants.NIBPWaveCategories.FirstOrDefault();
+
+                model.Layouts[1].MainWaveCategories = Constants.MainWaveCategories.Skip(2).Take(4).ToList();
+                model.Layouts[1].OtherWaveCategories = Constants.otherWaveCategories.Skip(2).Take(7).ToList();
+                model.Layouts[1].NIBPWaveCategory = Constants.NIBPWaveCategories.FirstOrDefault();
+
+                model.Layouts[2].MainWaveCategories = Constants.MainWaveCategories.Skip(4).Take(3).ToList();
+                model.Layouts[2].OtherWaveCategories = Constants.otherWaveCategories.Skip(8).Take(3).ToList();
+                model.Layouts[2].NIBPWaveCategory = Constants.NIBPWaveCategories.FirstOrDefault();
+
+                model.Layouts[3].MainWaveCategories = Constants.MainWaveCategories.Skip(5).Take(3).ToList();
+                model.Layouts[3].OtherWaveCategories = Constants.otherWaveCategories.Skip(11).Take(1).ToList();
+                model.Layouts[3].NIBPWaveCategory = Constants.NIBPWaveCategories.FirstOrDefault();
+            }
+
+            lbCustomLayout.ItemsSource = model.Layouts;
+
 
         }
 
+        /// <summary>
+        /// 版面选择按钮响应事件处理
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         private void Button_Click(object sender, RoutedEventArgs e)
         {
             Button button = sender as Button;
-
-            for (int i = 1; i <=4; i++)
+            for (int i = 1; i <= 4; i++)
             {
                 var grid = this.FindName("grid_" + i.ToString()) as Grid;
                 grid.Visibility = button.Tag.ToString() == i.ToString() ? Visibility.Visible : Visibility.Hidden;
             }
+            layoutSelectedIndex = int.Parse(button.Tag.ToString()) - 1;
 
+            spInitStatus.Opacity = 0;
+            selectedWave = null;
 
+            layoutName = model.Layouts[layoutSelectedIndex].Name;
+            tbLayoutName.Text = layoutName;
         }
 
+        /// <summary>
+        /// 版面位置按钮响应事件处理
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         private void Button_Click_1(object sender, RoutedEventArgs e)
         {
             Button button = sender as Button;
-            string btnText = (button.Content as TextBlock).Text;
-            if (btnText.StartsWith("波形"))
+            //读出按钮的坐标信息
+            string[] tags = button.Tag.ToString().Split(',');
+            waveCategoryIndex = int.Parse(tags[0]);
+            waveSelectedIndex = int.Parse(tags[1]);
+
+            //根据按钮的坐标信息,找到对应的波形数据以及 可供选择的波形类别
+            if (waveCategoryIndex == 0)
             {
                 waveCategories = Constants.MainWaveCategories;
+                selectedWave = model.Layouts[layoutSelectedIndex].MainWaveCategories[waveSelectedIndex];
             }
-            else if (btnText.StartsWith("视窗"))
+            else if (waveCategoryIndex == 1)
             {
                 waveCategories = Constants.otherWaveCategories;
+                selectedWave = model.Layouts[layoutSelectedIndex].OtherWaveCategories[waveSelectedIndex];
             }
             else
             {
                 waveCategories = Constants.NIBPWaveCategories;
+                selectedWave = model.Layouts[layoutSelectedIndex].NIBPWaveCategory;
             }
-
+            
+            // 刷新界面,显示波形的初始化状态选择控件
             lbWaveCategory.ItemsSource = waveCategories;
-            lbWaveCategory.SelectedValue = model.Layouts[0].MainWaveCategories[1];
+            lbWaveCategory.SelectedValue = selectedWave.Name;      
+            
+            spInitStatus.Opacity = 1;
+            rbInitStatus.DataContext = selectedWave;
+        }
+
+        /// <summary>
+        /// 保存设定
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void Button_Click_2(object sender, RoutedEventArgs e)
+        {           
+            Setting.Save(model);
+        }
+
+        /// <summary>
+        /// 更改版面位置对应波形响应事件处理
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void lbWaveCategory_Selected(object sender, RoutedEventArgs e)
+        {
+            if (lbWaveCategory.SelectedValue == null || selectedWave==null)
+            {
+                return;
+            }         
+            selectedWave.Name = lbWaveCategory.SelectedValue.ToString();
+            
+        }
+
+        /// <summary>
+        /// 选择波形的初始化状态修改,手工同步数据源
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void RadioButton_Checked(object sender, RoutedEventArgs e)
+        {
+            selectedWave.Status = (sender as RadioButton).Content.ToString();
+        }
+
+        /// <summary>
+        /// 版面名称的修改,手工同步数据源
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void tbLayoutName_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            string text = (sender as TextBox).Text;
+            if (!string.IsNullOrWhiteSpace(text))
+            {
+                model.Layouts[layoutSelectedIndex].Name = text;
+            }
+        }
+
+        /// <summary>
+        /// 当前选择版面设置为默认版面
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void Button_Click_3(object sender, RoutedEventArgs e)
+        {
+            if (layoutSelectedIndex < 0)
+            {
+                MessageBox.Show("请先选择一种版面");
+            }
+            for (int i = 0; i < model.Layouts.Count;i++)
+            {
+                model.Layouts[layoutSelectedIndex].IsDefault = i == layoutSelectedIndex;
+            }
+            Setting.Save(model);
+        }
+
+        private void Button_Close(object sender, RoutedEventArgs e)
+        {
+            this.Close();
         }
     }
 }
